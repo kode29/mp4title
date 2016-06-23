e0c2521112b860511761e7ff92468977f9cc9f2b
Kyle M. Perkins <kyle@q5industries.com>
2016-06-23 00:48:58 -0400

    first commit - upload to Github

---
+++ mp4title.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -   
+#title          :title-header.sh
+#description    :A script to automatically update the MP4 metadata title with the filename
+#author         :Kyle M. Perkins
+#date           :20160623
+#version        :0.1    
+#usage          :./title.sh
+#notes          :       
+#bash_version   :4.3.42(1)-release
+#============================================================================
+
+#!/bin/bash
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+for f in $(find . -name "*.mp4")
+do
+	#echo "Filepath: $f"
+	# Get filename (minus extension)
+	filenameExt="${f##*/}"
+	filename="${filenameExt%.*}"
+	echo -n "Processing $filename..."
+
+	# Get title (if exists)
+	read NAME <<< $(mp4info $f | awk '/Name/ { print $0 }')
+	NAME=${NAME:7}
+
+	# Compare title and filename. If !=, set. If not, pass
+	#echo "\nCompare: '$NAME' and '$filename'"; # Debug
+	if [ "$NAME" != $filename ]; then
+		echo "Updating title to \"$filename\"";
+		mp4tags -s "$filename" $f
+	else
+		echo "Title is the same. No action taken."
+	fi
+done
+echo "====";
+espeak -s 125 "All file tags modified"
+IFS=$SAVEIFS
